ab4c58322547262cd08bd9c6f8f5a980631647ef
Zakaria-CPU301 <ramadanzaka27@gmail.com>
2026-08-22 23:49:23 +0700

    enhance Navbar with keyword state and processing handling; adjust SmoothTableHead z-index; remove unused Button import

--- resources/js/Components/Partials/FormOverlay.jsx
+++ resources/js/Components/Partials/FormOverlay.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const FormOverlay = ({children}) => {
+const FormOverlay = ({children, handleSubmitForm}) => {
     return (
-        <div className="w-full h-full p-4 overflow-y-scroll overscroll-y-contain scrollbar-thumb-blue-500">
+        <form onSubmit={handleSubmitForm} className="w-full h-full p-4 overflow-y-scroll overscroll-y-contain scrollbar-thumb-blue-500">
             {children}
-        </div>
+        </form>
     );
 };
 
--- resources/js/Components/Partials/Table.jsx
+++ resources/js/Components/Partials/Table.jsx
@@ -1,55 +1,150 @@
+import { useState } from "react";
+import Dropdown from "../../Context/Dropdown";
+import Button from "../Elements/Button";
 import SmoothTableHead from "../Elements/SmoothTableHead";
 import StandardTableHead from "../Elements/StandardTableHead";
+import OverlayModal from "./OverlayModal";
+import FormBalance from "../Form/FormBalance";
+import ModalHeader from "../Elements/ModalHeader";
+import Card from "./Card";
+import { usePage } from "@inertiajs/react";
 
-const Table = ({ firstAction = null, columns, datas, iconEmpty, reason, className }) => {
+const Table = ({
+    firstAction = null,
+    columns,
+    datas,
+    iconEmpty,
+    reason,
+    customerDatas = null,
+    className,
+}) => {
     const result = (obj, keyName) =>
         keyName.split(".").reduce((acc, key) => acc[key], obj);
 
-    const currentRoute = route().current();
+    const { url } = usePage();
+
+    const [showOverlay, setShowOverlay] = useState(false);
+    const toggleOverlay = () => {
+        setShowOverlay(!showOverlay);
+    };
+
+    const [method, setMethod] = useState();
+
+    const [repoData, setRepoData] = useState([]);
 
     return (
-        <div className="w-full">
-            <table className="border-collapse w-full bg-main-table rounded-b-2xl">
-                {currentRoute === "category.index" ||
-                currentRoute === "customer.index" ? (
-                    <StandardTableHead columns={columns} />
-                ) : (
-                    <SmoothTableHead columns={columns} />
-                )}
+        <>
+            <div className="w-full">
+                <table className="border-collapse w-full bg-main-table rounded-b-2xl">
+                    {route().current("customer.index") ||
+                    route().current("category.index") ? (
+                        <StandardTableHead columns={columns} />
+                    ) : (
+                        <SmoothTableHead columns={columns} />
+                    )}
 
-                <tbody className="text-white">
-                    {datas.length ? (
-                        datas.map((data, index) => (
-                            <tr key={index}>
-                                <td className="text-center p-3">
-                                    {firstAction ?? index + 1}
-                                </td>
-                                {columns.map((col, i) => (
-                                    <td
-                                        className={`${col.opsionalClassName ?? "text-center"} p-3`}
-                                        key={i}
-                                    >
-                                        {result(data, col.key)}
+                    <tbody className="text-white">
+                        {datas.length ? (
+                            datas.map((data, index) => (
+                                <tr key={index}>
+                                    <td className="text-center p-3">
+                                        {firstAction ?? index + 1}
                                     </td>
-                                ))}
-                                <td className="text-center p-3">
-                                    <i className="bi bi-three-dots"></i>
+                                    {columns.map((col, i) => (
+                                        <td
+                                            className={`${col.opsionalClassName ?? "text-center"} p-3`}
+                                            key={i}
+                                        >
+                                            {result(data, col.key)}
+                                        </td>
+                                    ))}
+                                    <td className="text-center p-3">
+                                        <Dropdown>
+                                            <Dropdown.Trigger>
+                                                <i className="bi bi-three-dots cursor-pointer"></i>
+                                            </Dropdown.Trigger>
+                                            <Dropdown.Content
+                                                width="w-30"
+                                                contentClasses="py-2 bg-light-sky"
+                                                z={"z-52"}
+                                            >
+                                                <div className="flex flex-col items-center capitalize justify-start space-y-3  text-black">
+                                                    <Button
+                                                        className={""}
+                                                        clickFunc={() => {
+                                                            toggleOverlay();
+                                                            setMethod("edit");
+                                                            setRepoData(data)
+                                                        }}
+                                                    >
+                                                        <i
+                                                            className={`bi bi-pencil text-xl`}
+                                                        ></i>
+                                                        edit
+                                                    </Button>
+                                                    <Button
+                                                        className={""}
+                                                        clickFunc={() => {
+                                                            toggleOverlay();
+                                                            setMethod("hapus");
+                                                        }}
+                                                    >
+                                                        <i
+                                                            className={`bi bi-trash text-xl`}
+                                                        ></i>
+                                                        hapus
+                                                    </Button>
+                                                </div>
+                                            </Dropdown.Content>
+                                        </Dropdown>
+                                    </td>
+                                </tr>
+                            ))
+                        ) : (
+                            <tr>
+                                <td colSpan={100}>
+                                    <div className="flex flex-col justify-center items-center h-52 gap-4">
+                                        <i
+                                            className={`${iconEmpty ? iconEmpty : "bi bi-database-fill-x"} text-5xl`}
+                                        ></i>
+                                        <span className="text-3xl font-extrabold">
+                                            {reason ??
+                                                "Tidak ada data ditemukan"}
+                                        </span>
+                                    </div>
                                 </td>
                             </tr>
-                        ))
-                    ) : (
-                        <tr>
-                            <td colSpan={100}>
-                                <div className="flex flex-col justify-center items-center h-52 gap-4">
-                                    <i className={`${iconEmpty ? iconEmpty : 'bi bi-database-fill-x'} text-5xl`}></i>
-                                    <span className="text-3xl font-extrabold">{reason ?? 'Tidak ada data ditemukan'}</span>
-                                </div>
-                            </td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-        </div>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+
+            {showOverlay && (
+                <OverlayModal clickFunc={toggleOverlay}>
+                    <Card className="bg-powderblue w-4/5 md:w-2/3 min-h-0 px-4 max-h-[calc(80vh)] rounded-2xl">
+                        {url === "/balance" && (
+                            <>
+                                <ModalHeader
+                                    title={"edit nomor"}
+                                    clickFunc={toggleOverlay}
+                                />
+
+                                <FormBalance>
+                                    {method === "edit" ? (
+                                        <FormBalance.Update
+                                            customerDatas={customerDatas}
+                                            dataEdit={repoData}
+                                        />
+                                    ) : (
+                                        <FormBalance.Delete />
+                                    )}
+                                </FormBalance>
+                            </>
+                        )}
+                    </Card>
+                </OverlayModal>
+            )}
+        </>
     );
 };
 
