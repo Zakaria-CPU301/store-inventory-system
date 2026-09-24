8247021008e6dc42c0e1d09dffbeb480fe567cd6
Zakaria-CPU301 <ramadanzaka27@gmail.com>
2026-09-24 23:02:10 +0700

    feat: enhance Product and Transaction controllers with improved data handling and validation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,50 +4,81 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\Transaction;
+use App\Models\TransactionLog;
+use App\Models\Unit;
+use App\Models\UnitProduct;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules\File;
 use Inertia\Inertia;
 
 class ProductController extends Controller
 {
-    public function filters(Object $query, array $discovery) {
-        return $query->where(['product_name', 'description'], $discovery);
+    public function filters(Object $query, array $discovery)
+    {
+        return $query->where(['name', 'description'], $discovery);
     }
-    
+
     public function index(Request $request)
     {
-        $datas = Product::with(['categories']);
+        $datas = Product::with(['category']);
         if ($request->discovery || session('discovery')) {
             $datas = $this->filters($datas, $request->discovery);
         }
-        
+
         return Inertia::render('Main/Product', [
             'productDatas' => $datas->get(),
-            'categoryDatas' => Category::whereNotIn('category_name', ['token', 'dana', 'pulsa', 'paket data'])->get()
+            'categoryDatas' => Category::whereNotIn('category_name', ['token', 'dana', 'pulsa', 'paket data'])->get(),
+            'unitDatas' => Unit::all()
         ]);
     }
 
     public function store(Request $request)
     {
+        // dd($request);
         $validate = $request->validate([
-            'title' => ['required'],
+            'barcode' => 'nullable',
+            'title' => ['required', 'max:225'],
+            'units' => 'required',
+            'units.*.unit' => ['required'],
+            'units.*.qty' => ['required', 'numeric', 'min:0'],
+            'units.*.price' => ['required', 'numeric', 'min:500'],
             'image' => ['nullable', File::image()->max('500kb')],
-            'qty' => ['nullable', 'numeric', 'min:0'],
-            'category' => ['required'],
+            'category' => ['nullable'],
             'desc' => ['nullable'],
-            'price' => ['required', 'numeric', 'min:0']
+        ], [
+            'title.required' => 'nama produk tidak boleh kosong',
+            'title.max' => 'nama produk terlalu panjang',
+            'units.*.unit.required' => 'satuan tidak boleh kosong',
+            'units.*.qty.required' => 'jumlah produk tidak boleh kosong',
+            'units.*.price.required' => 'jumlah produk tidak boleh kosong',
+            'units.*.price.min' => 'harga produk minimal Rp.500'
+        ]);
+        // foreach($request->units as $unit) ;
+dd($request->units);
+        $path = null;
+        if ($request->file('image')) {
+            $path = $request->file('image')->store('image-products', 'public');
+            $path = str_replace('image-products/', '', $path);
+        }
+
+        $category = Category::firstOrCreate(['category_name' => $validate['category']], ['category_name' => $validate['category']]);
+
+        $product = Product::create([
+            'barcode' => (int) ($validate['barcode']),
+            'name' => $validate['title'],
+            'product_image' => $path,
+            'description' => $validate['desc'],
+            'category_id' => $category->id,
         ]);
 
-        $path = $request->file('image')->store('image-products', 'public');
-        $path = str_replace('image-products/', '', $path);
+        // UnitProduct::create(['product_id' => $product->id, 'unit_id' => $re]);
 
-        Product::create([
-            'product_name' => $validate['title'],
-            'product_image' => $path,
-            'qty' => $validate['qty'],
-            'price' => $validate['price'],
-            'description' => $validate['desc'],
-            'category_id' => $validate['category'],
+        $invoice = Transaction::where('qrcode', Inertia::getShared('codeScan'))->first();
+        TransactionLog::create([
+            'transaction_id' => $invoice->id,
+            'invoice_log_type' => Product::class,
+            'invoice_log_id' => $product->id,
         ]);
     }
 }
--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,19 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Transaction;
+use App\Models\TransactionLog;
+use App\Models\Unit;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Illuminate\Support\Str;
 
 class TransactionController extends Controller
 {
-    public function index() {
+    public function index(Request $request)
+    {
+        $transaction = Transaction::query();
+        if (!$transaction->where('printing', true)->exists()) {
+            $this->createInvoice($transaction, false);
+        } else if ($request->discovery) {
+            session()->put('codeScan', $request->discovery['invoice']);
+            Inertia::share('codeScan', $request->discovery['invoice']);
+        } else if (!session('codeScan')) {
+            $latestInvoice = Transaction::latest()->first()->qrcode;
+            session()->put('codeScan', $latestInvoice);
+            Inertia::share('codeScan', $latestInvoice);
+        }
+
         return Inertia::render('Main/Transaction', [
-            
+            'datas' => $transaction->with(['transactionLogs.invoice_log'])->where('printing', true)->latest()->get(),
+            'categoryDatas' => Category::whereNotIn('category_name', ['pulsa', 'dana', 'paket data', 'listrik'])->get(),
+            'unitDatas' => Unit::all()
         ]);
     }
 
-    public function scanning (Request $request) {
-        // dump($request->codeScan);
+    public function scanning(Request $request)
+    {
+        $invoice = Transaction::where('qrcode', 'LIKE', "{$request->code}")->first();
+        $product = Product::where('barcode', $request->code)->first();
+        $qrcodeInvoice = str_contains($request->code, 'faktur-');
+        if ($qrcodeInvoice) {
+            $invoice->update(['printing' => true]);
+        } else {
+            if (ctype_digit($request->code)) {
+                if ($product) {
+                    $invoice = Transaction::where('qrcode', Inertia::getShared('codeScan'))->first();
+                    TransactionLog::create([
+                        'transaction_id' => $invoice->id,
+                        'invoice_log_type' => Product::class,
+                        'invoice_log_id' => $product->id
+                    ]);
+                } else {
+                    Inertia::flash('barcode', $request->code);
+                }
+            } else {
+                $classname = 'bg-red-500';
+                $icon = 'x-circle';
+                $success = 'QRCode tidak terdaftar, silahkan pindai yang lain!';
+                Inertia::flash(['success' => $success, 'icon' => $icon, 'classname' => $classname]);
+            }
+        }
+
         return back();
+    }
+
+
+    public function invoice(Request $request)
+    {
+        $this->createInvoice(Transaction::query(), $request->incoming);
+    }
+
+    public function createInvoice(Object $transaction, bool $mutationMethod)
+    {
+        $getLatestId = Transaction::latest()->first()?->id + 1 ?? 1;
+        $hashId = Str::uuid();
+        $makeQRCode = "faktur-{$getLatestId}.{$hashId}";
+        $transaction->create([
+            'qrcode' => $makeQRCode,
+            'incoming' => $mutationMethod,
+            'printing' => true
+        ]);
+        Inertia::share('codeScan', $makeQRCode);
+        session()->put('codeScan', $makeQRCode);
     }
 }
